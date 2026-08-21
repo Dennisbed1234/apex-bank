e01b0b5ff2eeda7c1c9bb85049391888a656e6a7
Dennis Bed <dennisbed1234@gmail.com>
2026-08-21 07:09:31 -0400

    Update index.ts

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,10 @@
-import { drizzle } from 'drizzle-orm/neon-serverless'
-import { Pool } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
 import * as schema from './schema'
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  max: 1,
 })
 
 export const db = drizzle(pool, { schema })
